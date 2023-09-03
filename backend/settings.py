d54cc68f97fc6bb9b67c2ac1819e92e6e32676b6
Ahmed Bhiri <123560356+AhmedBhiri@users.noreply.github.com>
2023-09-03 16:52:10 +0100

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-0tm#y%q&5kh2!dc6cm1r#48fpqxd3crr57ava&z&-s1_&v61pt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = []
 
 
 # Application definition
